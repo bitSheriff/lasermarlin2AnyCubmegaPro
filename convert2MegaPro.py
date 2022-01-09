6167d95f6fc979fe1877906401cf2d365005d721
bitSheriff <bitsheriff@protonmail.com>
2022-01-09 14:01:39 +0100

    first draft of script

--- convert2MegaPro.py
+++ convert2MegaPro.py
@@ -2,14 +2,37 @@ import sys, pathlib
 import argparse
 
 # configure the argument parser for the cli usage
-parser = argparse.ArgumentParser(description="Arguments for the converter.")
+parser = argparse.ArgumentParser(description="Converter for the laser module of the Anycubic Mega Pro", add_help=True)
 parser.add_argument('--input', '-i', type= pathlib.Path, action='store', help="Input file which should be converted", required=True)
 parser.add_argument('--output', '-o', type= pathlib.Path, action='store', help="(optional) Output file where the result should be written to")
+parser.add_argument('--home', action='store_true', help="(optional) Homing before program start", default=False)
+parser.add_argument('--height', '-H', type=int, action='store', help="height of the workpiece [mm]", required=True)
+parser.add_argument('--focus','-f', type=int, action='store', help="(optional) Focus height of the laser [mm]", default=50)
 
 
 
+# Constants
+HEADER = "\
+; ---- MARLIN 2 ANYCUBIC MEGA PRO ---- \n\
+; converted by lasermarlin2AnyCubMegaPro (https://github.com/bitSheriff/lasermarlin2AnycubMegaPro) "
+CMD_HOMEING_ALL = "G28"
+CMD_MARLIN_LASER = "M106"
+CMD_ANYCUBIC_LASER = "G4 P0\nG6"
+CMD_SETZERO_Z = "G92 Z0"
+CMD_MOVE_Z = "G1 Z"
 
 
+def getStartConfig(homeing, focus, workpiece):
+    config = HEADER + "\n"
+    height = focus + workpiece
+    # move to home if wanted
+    if homeing is True:
+        config += CMD_HOMEING_ALL + "\n"
+    # move z to right height
+    config += str(CMD_MOVE_Z) + str(height) + "\n"
+    # reset the zero position
+    config += CMD_SETZERO_Z + "\n"
+    return config
 
 if __name__ == '__main__':
     # pares the given arguments
@@ -29,4 +52,21 @@ if __name__ == '__main__':
     with inputFile.open('r') as file:
         inputStream = file.read()
 
-    print(inputStream)
+    # check if the given file was already converted
+    if (inputStream.find(HEADER) != -1):
+        print("The given file was already converted")
+        exit()
+
+    # get the generated string from the user arguments
+    configString = getStartConfig(homeing=args.home, focus= args.focus, workpiece=args.height)
+
+    # new gcode which will be written to the output file
+    outputStream = ""
+    # insert the configuration at beginning
+    outputStream += configString
+    # replace the generic marlin laser commands with the commands Anycubic uses
+    outputStream += inputStream.replace(CMD_MARLIN_LASER, CMD_ANYCUBIC_LASER)
+
+    # write the file to the output file 
+    with outputFile.open('w+') as file:
+        file.write(outputStream)
